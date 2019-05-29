26b7e7c2ced3bd344778c82028fdf8357a5bbf1b
LilyMart7 <lilymart7@gmail.com>
2019-05-29 12:23:24 +0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,5 @@
+var taskInput = document.getElementById("new-task");//Add a new task.
+var addButton = document.getElementById("addButton");
+var incompleteTaskHolder = document.getElementById("incomplete-tasks");//ul of #incomplete-tasks
+var completedTasksHolder = document.getElementById("completed-tasks");//completed-tasks
+
